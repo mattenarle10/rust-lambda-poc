5205a1bf7c5e89c1bc4bea0de923786ceb6848f4
Matthew Enarle <enarlem10@gmail.com>
2025-11-19 14:12:18 +0800

    feat: rust ownership and performance demo

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,24 @@ fn classify_number(n: i32) -> &'static str {
     }
 }
 
+fn shout(message: String) -> String {
+    format!("{}!!!", message.to_uppercase())
+}
+
+fn print_message(message: &str) {
+    println!("msg: {message}");
+}
+
 fn main() {
     println!("Hello, world!");
 
     println!("5 is {}", classify_number(5));
     println!("-3 is {}", classify_number(-3));
     println!("0 is {}", classify_number(0));
-    println!("10 is {}", classify_number(10)
-);
+    println!("10 is {}", classify_number(10));
+
+    let msg = String::from("hello rust");
+    let loud = shout(msg);
+    print_message(&loud);
+    println!("loud = {loud}");
 }
